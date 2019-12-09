d92e8d86c228f051e7f3c4d401df6d0443e838f9
AnuTangirala <37104741+AnuTangirala@users.noreply.github.com>
2019-12-09 10:58:14 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,71 +3,114 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace FileValidation
 {
     class Program
     {
-        static void Main(string[] args){
-
-            var str = File.ReadAllText(@"", Encoding.UTF8);
-            var fileFormat = JsonConvert.DeserializeObject<List<FileFormmat>>(str);
-
-            int lineCounter=0, noOfMaxOccurencesAllowed=1;
-            string prevSequence = "BOF";
-            string currSequence;
-            bool flag = false;
-            var checkSeq = new FileFormat();
-
-            var fileStream = new FileStream(@"", FileMode.Open, FileAccess.Read);
-            using( var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+        static void Main(string[] args)
+        {
+            if(args.Length < 2)
             {
-                string line;
-                while((line = streamReader.ReadLine()) != null){
-                    lineCounter++;
-                    if(line.Length > 0)
-                    {
-                        currSequence = line.SubString(0, 4).Trim();
-                        checkSeq = fileFormat.Where( x => x.StartingSequence == prevSequence).FirstOrDefault();
-                        if(prevSequence != "BOF"){
-                            if(prevSequence == currSequence){
-                                noOfMaxOccurencesAllowed++;
-                                if(checkSeq != null && noOfMaxOccurencesAllowed > checkSeq.NoOfTimesExpected)
-                                {
-                                    flag = true;
-                                    Console.WriteLine("File is not in correct format at line: " + lineCounter);
-                                    break;
-                                }
-                            }
-                            else{
-                                noOfMaxOccurencesAllowed = 0;
-                            }
-                        }
-                        if(checkSeq != null && checkSeq.CanBeFollowedBySequence.Any(x=> x.Contains(currSequence)))
-                        {
-                            prevSequence = currSequence;
-                        }
-                        else{
-                            flag = true;
-                            Console.WriteLine("File is not in correct format at line: " + lineCounter);
-                            break;
-                        }
-                        Console.WriteLine(line.TrimStart());
-                    }
-                }
-                if(!flag){
-                    Console.WriteLine("The file is in expected format");
-                }
+                Console.WriteLine("File path and Record Looping Path are required");
+                return;
             }
-            Console.ReadLine();
+           
+            var filePath = args[0];
+            var recordLoopingFilePath = args[1];
 
+           if (File.Exists(filePath) && File.Exists(recordLoopingFilePath))
+            {               
+                var fileFormatStr = File.ReadAllText(recordLoopingFilePath, Encoding.UTF8);
+                var fileFormat = JsonConvert.DeserializeObject<List<FileFormat>>(fileFormatStr);
+
+                int lineCounter = 0; // to track on which line number the format is not as expected
+                int noOfMaxOccurencesAllowed = 1; // no of times the record type can occur in a sequence
+                string prevSequence = "BOF"; // To represent the beginning of file
+                string currSequence;
+                bool flag = false;
+                bool needsFollowUpRecord = true; // to check if a rcord type needs to be followed up by another record type
+                var checkSeq = new FileFormat();
+
+                var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+                try
+                {
+                    using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                    {
+                        string line;
+                        while ((line = streamReader.ReadLine()) != null)
+                        {
+                            lineCounter++;
+                            if (line.Trim().Length > 0)
+                            {
+                                currSequence = line.TrimStart().Substring(0, 4).Trim();
+
+                                checkSeq = fileFormat.Where(x => x.StartingSequence == prevSequence).FirstOrDefault();
+
+                                if (prevSequence == currSequence)
+                                    {
+                                        noOfMaxOccurencesAllowed++;
+                                        if (checkSeq != null && noOfMaxOccurencesAllowed > checkSeq.NoOfTimesExpected)
+                                        {
+                                            flag = true;
+                                            Console.WriteLine("File is not in correct format at line: " + lineCounter);
+                                            break;
+                                        }
+
+                                    }
+                                    else
+                                    {
+                                        noOfMaxOccurencesAllowed = 0;
+
+                                        if (checkSeq != null && checkSeq.CanBeFollowedBySequence.Any(x => x.Contains(currSequence)))
+                                        {
+                                            prevSequence = currSequence;
+                                            needsFollowUpRecord = fileFormat.Where(x => x.StartingSequence == prevSequence).Select(x => x.NeedsFollowUpRecord).First();
+                                        }
+                                        else
+                                        {
+                                            flag = true;
+                                            Console.WriteLine("File is not in correct format at line: " + lineCounter);
+                                            break;
+                                        }
+                                    }
+                            }                              
+
+                        }
+                        if (!flag && !needsFollowUpRecord)
+                        {
+                            Console.WriteLine("The file is in expected format");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The file is not in expected format at line: " + lineCounter);
+                        }
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("An exception occured: " + ex.Message);
+                }               
+                
+            }
+           else
+            {
+                var message = File.Exists(filePath) ? "Record Looping file is not present" : "File is not present";
+                Console.WriteLine(message);
+            }
+            Console.ReadLine(); 
         }
     }
-    public class FileFormat{
-        public string StartingSequence { get; set;}
-        public int NoOfTimesExpected { get; set;}
-        public List<String> CanBeFollowedBySequence { get; set;}
-    }
 
+    public class FileFormat
+    {
+        public string StartingSequence { get; set; }
+        public int NoOfTimesExpected { get; set; }
+        public bool NeedsFollowUpRecord { get; set; }
+        public List<string> CanBeFollowedBySequence { get; set; }
+    }
 }
